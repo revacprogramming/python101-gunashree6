810fab6a7e7a240fe98a24d205dab6d9f7bb1724
gunashree6 <sguna1415@gmail.com>
2022-05-24 19:29:28 +0530

    completed

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,17 @@
 # Tuples
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+d=dict()
+for line in handle:
+    if not line.startswith("From "):
+        continue
+    l=line.split()
+    l=l[5]
+    l=l[0:2]
+    d[l]=d.get(l,0)+1
+tem=sorted([(v,c) for v,c in d.items()])
 
-filename = "dataset/mbox-short.txt"
+for v,c in tem:
+    print(v,c)
--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -1,2 +1,9 @@
 # Network Programming
 # https://www.py4e.com/lessons/network
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+ 
+dirty_dozen = [fruits, vegetables]
+ 
+print(dirty_dozen[0][2])
