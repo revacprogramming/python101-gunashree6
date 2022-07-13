dc0ad81facd2a08b49db67b2fb6fcb38cf48a261
gunashree6 <sguna1415@gmail.com>
2022-07-13 16:00:12 +0530

    comp;leted

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,2 +1,13 @@
 # Using Web Services
 # https://www.py4e.com/lessons/servces
+import urllib.request
+import xml.etree.ElementTree as et
+url=input('enter url :')
+fh=urllib.request.urlopen(url).read()
+tree= et.fromstring(fh)
+data=tree.findall('comments/comment')
+sum=0
+for i in data:
+  num=int(i.find('.//count').text)
+  sum+=num
+print(sum)
