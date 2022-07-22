eff00b407700e323c53ee55b0acb8a1b5b6e0403
gunashree6 <sguna1415@gmail.com>
2022-07-22 20:09:06 +0530

    completed

--- ActivitySet#2/problem#3.py
+++ ActivitySet#2/problem#3.py
@@ -7,20 +7,27 @@ def get_cs():
 
 
 def cs_to_lot(cs):
-    a=[cs]
-    g=a.split()
-    print(g)
+    data =list()
+    a=cs.split(";")
+    if "" in a:
+        a.remove('')
+    for ele in a:
+        t=ele.split("=")
+        t=tuple(t)
+        data.append(t)
+    return data    
 
 
-   
+    
 
 
 def main():
     cs = get_cs()
 
     lot = cs_to_lot(cs)
-   
+    print(lot)
 
 
 if __name__ == '__main__':
     main()
+                
